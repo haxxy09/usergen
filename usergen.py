df966dcbaf59f139fdff49208ff73572cf3e633c
ibwighane <bsmwalwanda@gmail.com>
2021-01-28 07:19:58 +0200

    coding convention on point, happy and sad paths

--- usergen.py
+++ usergen.py
@@ -13,10 +13,7 @@ filename = config['DHIS2_USERS_FILENAME']
 
 auth = HTTPBasicAuth(username, password)
 
-# always have a way of getting the file from
 reader = tuple(csv.DictReader(open(filename)))
-
-# TODO: environment variables need to be wired up
 
 
 def generate_ids(count):
@@ -40,7 +37,7 @@ def get_users_for_group(group_id):
         return ''
 
 
-def getResource(name, key):
+def get_resource(name, key):
     try:
         req = requests.get('https://play.dhis2.org/2.35.1/api/{0}.json?paging=false'.format(name),
                            auth=auth)
@@ -52,7 +49,7 @@ def getResource(name, key):
         return []
 
 
-def createUsers(payload):
+def create_users(payload):
     try:
         req = requests.post('https://play.dhis2.org/2.35.1/api/metadata',
                             json=payload, auth=auth)
@@ -61,60 +58,61 @@ def createUsers(payload):
         print(e)
 
 
-def getUserRoles():
-    return getResource('userRoles', 'userRoles')
+def get_user_roles():
+    return get_resource('userRoles', 'userRoles')
 
 
-def getOrganisationUnits():
-    return getResource('organisationUnits', 'organisationUnits')
+def get_organisation_units():
+    return get_resource('organisationUnits', 'organisationUnits')
 
 
-def getUserGroups():
-    return getResource('userGroups', 'userGroups')
+def get_user_groups():
+    return get_resource('userGroups', 'userGroups')
 
 
-def getUserGroupUsers(id):
-    return getResource('userGroups'+'/{0}'.format(0), 'users')
-
-
-def getResourceId(name, resources):
+def get_resource_id(name, resources):
     for resource in resources:
         if resource['displayName'] == name:
             return resource['id']
 
 
 def create_user_groups(user_group_combos):
-    groupIds = set([group["groupId"] for group in user_group_combos])
-    user_groups = []
+    try:
+        group_ids = set([group["groupId"] for group in user_group_combos])
 
-    for groupId in groupIds:
-        unique_combos = filter(
-            lambda x: x["groupId"] == groupId, user_group_combos)
-        users_ids_in_memory = [combo["userId"] for combo in unique_combos]
-        mapped_users_ids_in_memory = map(
-            lambda x: {"id": x}, users_ids_in_memory)
-        users = get_users_for_group(
-            groupId) + mapped_users_ids_in_memory
+        user_groups = []
 
-        what_we_want = {
-            "name": filter(lambda x: x["groupId"] == groupId, user_group_combos)[0]["groupName"],
-            "id": groupId,
-            "users": users
-        }
-        user_groups.append(what_we_want)
-    return user_groups
+        for group_id in group_ids:
+            group_combos = filter(
+                lambda x: x["groupId"] == group_id, user_group_combos)
+            user_ids_in_memory = [combo["userId"] for combo in group_combos]
+            mapped_user_ids_in_memory = map(
+                lambda x: {"id": x}, user_ids_in_memory)
+            users = get_users_for_group(group_id) + mapped_user_ids_in_memory
+
+            user_group = {
+                "name": filter(lambda x: x["groupId"] == group_id, user_group_combos)[0]["groupName"],
+                "id": group_id,
+                "users": users
+            }
+            user_groups.append(user_group)
+        return user_groups
+    except Exception as e:
+        print('Exception in create user groups {0}'.format(e))
+        return []
 
 
 def create_user_list(entries):
-    organisationUnits = getOrganisationUnits()
-    userGroups = getUserGroups()
-    userRoles = getUserRoles()
+    organisation_units = get_organisation_units()
+    user_groups = get_user_groups()
+    user_roles = get_user_roles()
 
     total_users = sum(1 for r in reader)
     user_ids = generate_ids(total_users)
 
     users = []
     user_group_combos = []
+
     for index, row in enumerate(entries):
         user_id = user_ids[index]
         user = {
@@ -126,18 +124,18 @@ def create_user_list(entries):
                 "password": row['password'],
                 "userRoles": [
                     {
-                        "id": getResourceId(row['userRoles'], userRoles)
+                        "id": get_resource_id(row['userRoles'], user_roles)
                     }
                 ]
             },
             "organisationUnits": [
                 {
-                    "id": getResourceId(row['organisationUnits'], organisationUnits)
+                    "id": get_resource_id(row['organisationUnits'], organisation_units)
                 }
             ],
             "dataViewOrganisationUnits": [
                 {
-                    "id": getResourceId(row['dataViewOrganisationUnits'], organisationUnits)
+                    "id": get_resource_id(row['dataViewOrganisationUnits'], organisation_units)
                 }
             ]
         }
@@ -145,7 +143,7 @@ def create_user_list(entries):
         user_group_combo = {
             "userId": user_id,
             "groupName": row["userGroups"],
-            "groupId": getResourceId(row['userGroups'], userGroups)
+            "groupId": get_resource_id(row['userGroups'], user_groups)
         }
 
         user_group_combos.append(user_group_combo)
@@ -155,6 +153,7 @@ def create_user_list(entries):
 
 
 if __name__ == '__main__':
+
     print('Script running ...')
     users, combos = create_user_list(reader)
     user_groups = create_user_groups(combos)
@@ -163,4 +162,4 @@ if __name__ == '__main__':
         "userGroups": user_groups
     }
     print(payload)
-    # createUsers(payload) TODO: actual creating
+    # create_users(payload) TODO: actual creating
